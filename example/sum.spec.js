1d96457b70218dbe30105a86be5a7fbd9020ac9e
coderaiser <coderaiser@users.noreply.github.com>
2022-09-19 17:29:46 +0000

    chore: putout: actions: lint ☘️

--- example/sum.spec.js
+++ example/sum.spec.js
@@ -1,11 +1,11 @@
 import {test} from 'supertape';
 import {sum} from './sum.js';
 
-test('name: sum', t => {
-  const result = sum('a', 'b');
-  const expected = 'ab';
-  
-  t.equal(result, expected);
-  t.end();
+test('name: sum', (t) => {
+    const result = sum('a', 'b');
+    const expected = 'ab';
+    
+    t.equal(result, expected);
+    t.end();
 });
 
